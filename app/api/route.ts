f51e67b68e782576ba080bd84cb1f402110ee8f1
NeoNeqs <76450252+NeoNeqs@users.noreply.github.com>
2025-10-25 15:38:34 +0200

    Test webhook v5

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,12 +1,11 @@
-export async function POST(request: Request) {
+export async function GET(request: Request) {
     // const secret = process.env.REVALIDATE_SECRET;
-    const { path, secret: reqSecret } = await request.json();
     // if (reqSecret !== secret) {
     // return new Response("Invalid token", { status: 401 });
     // }
     console.log(request.headers);
-    const actionType = request.headers.get("action-type");
-    console.log(actionType);
-    return new Response("", { status: 200 });
-    // return new Response(`Revalidated ${path}`, { status: 200 });
+    // const actionType = request.headers.get("action-type");
+    // console.log(actionType);
+
+    return new Response(``, { status: 200 });
 }
