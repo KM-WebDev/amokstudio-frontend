1b161c8b36baee28376605aa1b2331983aa6d1b5
NeoNeqs <76450252+NeoNeqs@users.noreply.github.com>
2025-10-25 19:52:23 +0200

    Test webhook v17

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,3 +1,8 @@
-export default async function Portfolios() {
-    return <></>;
+import PortfolioDisplay from "@/components/Portfolio";
+import { client } from "@/services/sanity/client";
+import { PORTFOLIO_HOMEPAGE_QUERY } from "@/services/sanity/queries";
+
+export default async function Portfolio() {
+    const result = await client.fetch(PORTFOLIO_HOMEPAGE_QUERY);
+    return <PortfolioDisplay portflios={result}></PortfolioDisplay>;
 }
