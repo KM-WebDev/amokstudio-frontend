f3a8ecdb0bff3c6f73f8dc659e9799ad52fa1ac1
Michael <76450252+NeoNeqs@users.noreply.github.com>
2025-10-27 09:15:32 +0100

    add several hero bg alternatives to choose (#3)
    Co-authored-by: Jakub Pniewski <jakub.pniewski@icloud.com>

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -2,11 +2,12 @@ import Hero from "@/components/pages/home/Hero";
 import PageContent from "./PageContent";
 import Bento from "./Bento";
 import Logo from "@/components/Logo";
+import Hero2 from "./Hero copy";
 
 export default async function HomePage() {
     return (
         <>
-            <Hero />
+            <Hero2 />
             <PageContent>
                 <Bento />
                 <div className="h-[1000px]"></div>
